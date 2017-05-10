b838816f558aa496e1209c18f393985202338c75
Kasama <ralegro@bol.com.br>
2017-05-09 21:17:55 -0300

    removed typescript compiled files

--- dist/controllers/ClientController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ClientController {
-    getAllClients() {
-        return [];
-    }
-}
-exports.ClientController = ClientController;
--- dist/models/ClientModel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ClientModel {
-}
-exports.ClientModel = ClientModel;
-exports.default = ClientModel;
