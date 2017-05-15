e60d437d9d4d0a8ec8abb8150e5b95be53dcaa14
Kasama <ralegro@bol.com.br>
2017-05-15 02:54:38 -0300

    added static files to gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,30 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+const STATIC_FILES = ['src/public/**'];
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('scripts', () => {
-  const tsResult = tsProject.src()
-  .pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest('dist'));
+	const tsResult = tsProject.src()
+		.pipe(tsProject());
+	return tsResult.js.pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', ['scripts'], () => {
-  gulp.watch('src/**/*.ts', ['scripts']);
+	gulp.watch('src/**/*.ts', ['scripts']);
+	gulp.watch('src/public/**', ['static']);
 });
 
 gulp.task('assets', function() {
-  return gulp.src(JSON_FILES)
-  .pipe(gulp.dest('dist'));
+	return gulp.src(JSON_FILES)
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['watch', 'assets']);
+gulp.task('static', function() {
+	return gulp.src(STATIC_FILES)
+		.pipe(gulp.dest('dist/public'));
+});
+
+gulp.task('default', ['watch', 'assets', 'static']);
